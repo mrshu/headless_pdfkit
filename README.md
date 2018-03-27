1e8184344da4bdc70c1dc3d60c22b09686c8a70d
mr.Shu <mr@shu.io>
2018-03-27 16:17:23 +0200

    update: Markdown in long_description
    Signed-off-by: mr.Shu <mr@shu.io>

--- README.md
+++ README.md
@@ -7,6 +7,11 @@ to run. In Debian land, this generally requires `xvfb-run` wrapper.
 The `headless_pdfkit` package tries to make the [hotfix](https://github.com/JazzCore/python-pdfkit/issues/56#issuecomment-305593936)
 proposed by [jakewins](https://github.com/jakewins) a bit easier to work with.
 
+Installation
+------------
+
+    pip install headless-pdfkit
+
 Examples
 --------
 
--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ with open('README.md') as f:
     long_description = f.read()
 
 setup(name='headless_pdfkit',
-      version='0.1.1',
+      version='0.1.2',
       description='A headless version of pdfkit which makes use of xvfb-run',
       long_description=long_description,
+      long_description_content_type='text/markdown',
       url='http://github.com/mrshu/headless_pdfkit',
       author='Marek Suppa (mr.Shu)',
       author_email='mr@shu.io',
